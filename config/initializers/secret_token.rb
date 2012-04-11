dabf0be05ff1bfb1cc7441190629f47b10b876c8
RakeshManne <rakesh.manne520@gmail.com>
2012-04-11 11:03:21 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Appfive::Application.config.secret_token = '1bc5c7ea066eee2cd822ef4162a68e0f1059d08ba9ca334ce30a06eb93fa707879e2cbfb15f14f8b5988549d915a8b8249858dc5db54cf592d6bb621ffac07a8'
